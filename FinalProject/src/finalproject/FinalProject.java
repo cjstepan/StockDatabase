e36168399b95d27de27582faac5f9f4b18c468ff
cjstepan <35180378+cjstepan@users.noreply.github.com>
2023-04-29 18:52:42 -0500

    added design requirements for the menu

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -8,6 +8,17 @@ public class FinalProject
 {
 
     /**
+     * 1 - Company Statements
+     * 2 - StockPurchase Statements
+     * 3 - Person Statements
+     * 4 - AccountOwnership Statements
+     * 
+     * Create secondary menus for each one of these
+     * Create tertiary menus for each executed statement
+     */
+    
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) 
