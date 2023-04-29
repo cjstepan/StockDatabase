e28e56ae9e6e6eb333ad59ad3e52f1c98c33429c
cjstepan <35180378+cjstepan@users.noreply.github.com>
2023-04-29 18:46:56 -0500

    Formatting changes

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -4,13 +4,16 @@ package finalproject;
  *
  * @author Calvin Stepan
  */
-public class FinalProject {
+public class FinalProject 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Dao dao = new Dao();
+        
     }
     
 }
